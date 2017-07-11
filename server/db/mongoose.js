536bf48446426ec2dcbf92982c6d4a96678600f0
Juanganzo69 <juan1910@live.com.mx>
2017-07-10 22:57:45 -0500

    Añadido configuraciones de produccion y database de testeo

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://juanganzo69:Watusi04@ds153412.mlab.com:53412/node-api-todo' || 'mongodb://localhost:27017/TodoApp', { useMongoClient: true });
+mongoose.connect( process.env.MONGODB_URI, { useMongoClient: true });
 // mongoose.connect('mongodb://localhost:27017/TodoApp', { useMongoClient: true });
+
+
 
 module.exports = { mongoose };
