860ba594802e0c81d5f9ab981006a8647dd3fe1f
Juanganzo69 <juan1910@live.com.mx>
2017-07-10 22:58:54 -0500

    Añadido configuraciones de produccion y database de testeo

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect( process.env.MONGODB_URI, { useMongoClient: true });
-// mongoose.connect('mongodb://localhost:27017/TodoApp', { useMongoClient: true });
-
-
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useMongoClient: true });
 
 module.exports = { mongoose };
