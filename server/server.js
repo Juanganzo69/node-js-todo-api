86b686fb2dbf2a025c937698884634773190bd08
Juanganzo69 <juan1910@live.com.mx>
2017-07-13 19:24:00 -0500

    añadido GET /users/me y autentificacion middleware

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var bodyParse = require('body-parser');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+var { autentificacion } = require('./middleware/autenficacion');    
 
 var app = express();
 var port = process.env.PORT;
@@ -96,13 +97,31 @@ app.post('/users', ( req, res ) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
 
-    
-
     user.save().then( () => {
         return user.generarAuthToken();
     }).then( (token) => {
         res.header('x-auth', token).send(user);
     }).catch( (e) => res.status(400).send(e));
+});
+
+var autentificacion = ( req, res, next ) => {
+    var token = req.header('x-auth');
+
+    User.findByToken(token).then( (user) => {
+        if( !user ){
+            return Promise.reject();
+        }
+
+        req.user = user;
+        req.token = token;
+        next();
+    }).catch( (e) => {
+        res.status(401).send();
+    });
+}
+
+app.get('/users/me', autentificacion, ( req, res ) => {
+    res.send(req.user);
 });
 
 
