1326bf3d6fb4a63eb6e081add4f7fc3db0ad6438
Juanganzo69 <juan1910@live.com.mx>
2017-07-17 19:17:57 -0500

    configurado config para que se use un json

--- server/test/seed/seed.js
+++ server/test/seed/seed.js
@@ -25,7 +25,7 @@ const users = [{
     password: 'Watusi06',
     tokens:[{
         access: 'auth',
-        token: jwt.sign({ _id: userId, access: 'auth' }, '123abc').toString()
+        token: jwt.sign({ _id: userId, access: 'auth' }, process.env.JWT_SECRET).toString()
     }]
 }, {
     _id: userTwoId,
@@ -33,7 +33,7 @@ const users = [{
     password:'yselacreyo',
     tokens:[{
         access: 'auth',
-        token: jwt.sign({ _id: userTwoId, access: 'auth' }, '123abc').toString()
+        token: jwt.sign({ _id: userTwoId, access: 'auth' }, process.env.JWT_SECRET).toString()
     }]
 }];
 
